960049ecdc085087fd5b7bee36ff2c2f4292b6d7
indexman <ivaniohanness@gmail.com>
2025-03-13 07:20:12 +0100

    added new-branch

---
+++ hello.java
@@ -0,0 +1,9 @@
+// package flexisaf-wk9;
+
+ public class hello {
+    public static void main(String[] args) {
+        String greeting = "Hello, Interns!";
+        String greeting2 = "Welcome to Flexisaf's Backend Training";
+        System.out.println(greeting + " " + greeting2);
+    }
+}  
